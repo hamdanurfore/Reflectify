e8ca7c20e35af19bf3a243de0b101d33a80f0e0b
Hamda <nurfore14@gmail.com>
2024-02-19 19:56:15 +0000

    Navbar created with brand colours

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import './components/Navbar.jsx'
+import Navbar from './components/Navbar.jsx'
+
+const App = () =>
+{
+  return (
+    <>
+    <Navbar/>
+<div className='bg-primary w-full h-screen'></div>
+  </>
+  )
+}
+
+export default App
