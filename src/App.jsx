282af81bf95aab0246d9209042da9cabd91c1477
Hamda <nurfore14@gmail.com>
2024-02-28 00:01:21 +0000

    responsive hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import './components/Navbar/Navbar.jsx'
 import Navbar from './components/Navbar/Navbar.jsx'
-// import Hero from './components/Hero/Hero.jsx'
+import Hero from './components/Hero/Hero.jsx'
 // import Features from './components/Features/Features.jsx'
 /// import Demo from './components/Demo/Demo.jsx'
 // import Footer from './components/Footer/Footer.jsx'
@@ -12,7 +12,7 @@ const App = () =>
   return (
     <>
     <Navbar/>
-    {/* <Hero/> */}
+    <Hero/>
      {/* <Features/> */}
     {/*<Demo/>
     <Footer/> */}
